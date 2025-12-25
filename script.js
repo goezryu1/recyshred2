3d5ef211a39f577424c3a0f94beb2fb0f5935657
goezryu1 <miguelalfoznoherrera@gmail.com>
2025-12-26 00:01:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -56,3 +56,22 @@ if (document.querySelectorAll(".reward-card").length) {
     });
   });
 }
+  const toggleBtn = document.getElementById("theme-toggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+      toggleBtn.textContent = "☀️ Light Mode";
+      localStorage.setItem("theme", "dark");
+    } else {
+      toggleBtn.textContent = "🌙 Dark Mode";
+      localStorage.setItem("theme", "light");
+    }
+  });
+
+  // Load saved theme
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleBtn.textContent = "☀️ Light Mode";
+  }
